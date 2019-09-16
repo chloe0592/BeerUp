89c41233e94187f75317b110e33488c13b4ba9c9
chloe0592 <klaudija.ljevar@gmail.com>
2019-09-16 09:00:37 +0200

    initialisation

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h3>
+          hi hi!
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Header;
